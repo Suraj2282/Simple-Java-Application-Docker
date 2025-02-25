bbc37c03c6e27045a414c5daf3c78c176aaae0d5
Suraj2282 <Suraj.Thorat@cognizant.com>
2025-02-25 15:09:08 +0530

    Second Java Project

---
+++ docker/Docker P-2/src/Main.java
@@ -0,0 +1,14 @@
+import java.util.*;
+public class Main
+{
+    public static void main(String args[])
+    {
+        Date newDate = new Date();
+        long time =  newDate.getTime();
+
+        System.out.println("Hello Docker!!");
+        System.out.println(time);
+
+       
+    }
+}
